f909db8882a099a812d5442bbf30b4dcdc27b25d
mskoldSU <skoldmobile@gmail.com>
2022-08-02 14:45:40 +0200

    Extending usable range of accnr2id

--- R/ESB_accnr2id.R
+++ R/ESB_accnr2id.R
@@ -8,12 +8,19 @@
 ESB_accnr2id <- function(accnr){
   accnr <- stringr::str_remove(accnr, "/")
   nr <- stringr::str_sub(accnr, -5)
-  yr <- stringr::str_sub(accnr, -7, -6)
-  lst <- stringr::str_sub(accnr, 1, 1)
+  yr <- stringr::str_sub(accnr, -8, -6)
+  lst <- stringr::str_sub(accnr, 1, 1) |> toupper()
   prefix <- dplyr::case_when(
-    lst == "A" ~ "10",
-    lst == "B" ~ "20",
-    lst == "C" ~ "30"
+    lst == "A" ~ "1",
+    lst == "B" ~ "2",
+    lst == "C" ~ "3",
+    lst == "D" ~ "4",
+    lst == "G" ~ "7",
+    lst == "H" ~ "8",
+    lst == "L" ~ "5",
+    lst == "P" ~ "9",
+    lst == "X" ~ "6",
+    TRUE ~ "0"
   )
   id <- paste0(prefix, yr, nr)
   id
